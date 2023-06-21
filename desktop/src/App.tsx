6f4bab1dc968a4552d8dde19f6b250df50b1c3dd
Arciiix <artur.nowak100@gmail.com>
2023-06-21 20:13:59 +0200

    Init desktop app project

---
+++ desktop/src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+  return (
+    <div className="App">
+      <span>Hello Flify!</span>
+    </div>
+  );
+}
+
+export default App;
