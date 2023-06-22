ce6b3292e6b3b18ac64d0e78795ce2a642b4b1d3
Arciiix <artur.nowak100@gmail.com>
2023-06-22 22:42:48 +0200

    Add animated logo component

--- desktop/src/App.tsx
+++ desktop/src/App.tsx
@@ -1,10 +1,13 @@
 import { useState } from "react";
+import Logo from "./components/ui/Logo/Logo";
 
 function App() {
   const [count, setCount] = useState(0);
   return (
     <div className="App">
       <span>Hello Flify!</span>
+
+      <Logo isAnimated width={150} />
     </div>
   );
 }
---
+++ desktop/src/components/ui/Logo/Logo.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+
+// Linter throws an error, but it works (using svgr plugin in Vite)
+// @ts-ignore
+import { ReactComponent as LogoSvg } from "../../../assets/icons/Flify.svg";
+
+import "../../../styles/logo.css";
+
+interface LogoProps {
+  isSmall?: boolean;
+  isAnimated?: boolean;
+  width?: number;
+}
+
+export default function Logo({
+  isSmall = false,
+  isAnimated = false,
+  width = 100,
+}: LogoProps) {
+  return (
+    <div className={`${isAnimated ? "logo-animated" : ""} logo-wrapper`}>
+      {isSmall ? (
+        <LogoSvg width={width} />
+      ) : (
+        <div className="flex gap-2">
+          <span
+            className="text-flify"
+            style={{
+              fontSize: `${width}px`,
+            }}
+          >
+            Fl
+          </span>
+          <LogoSvg width={width} />
+          <span
+            className="text-flify"
+            style={{
+              fontSize: `${width}px`,
+            }}
+          >
+            fy
+          </span>
+        </div>
+      )}
+    </div>
+  );
+}
