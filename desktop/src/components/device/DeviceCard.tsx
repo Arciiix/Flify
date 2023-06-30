d94aac9755bb1350062d1e3d198c4db107a5515b
Arciiix <artur.nowak100@gmail.com>
2023-06-30 22:18:20 +0200

    Add signal indicator

--- desktop/src/components/device/DeviceCard.tsx
+++ desktop/src/components/device/DeviceCard.tsx
@@ -1,10 +1,13 @@
 import Battery from "../ui/Battery/Battery";
+import Signal from "../ui/Signal/Signal";
 
 export default function DeviceCard() {
   return (
-    <div className="flex flex-col bg-flify bg-opacity-20 p-4 rounded-xl">
-      <div>
+    <div className="flex flex-col bg-flify bg-opacity-20 p-4 rounded-xl w-72">
+      <div className="flex justify-around">
         <Battery percentage={50} />
+
+        <Signal latency={50} />
       </div>
     </div>
   );
---
+++ desktop/src/components/ui/Signal/Signal.tsx
@@ -0,0 +1,34 @@
+import { useMemo } from "react";
+// Linter throws an error, but it works (using svgr plugin in Vite)
+// @ts-ignore
+import { ReactComponent as SignalSvg } from "../../../assets/icons/signal.svg";
+import "./Signal.css";
+
+interface SignalProps {
+  latency: number;
+}
+
+type SignalStrength = 4 | 3 | 2 | 1;
+
+export default function Signal({ latency }: SignalProps) {
+  const signalStrength: SignalStrength = useMemo(() => {
+    if (latency < 20) {
+      return 4;
+    } else if (latency < 40) {
+      return 3;
+    } else if (latency < 80) {
+      return 2;
+    } else {
+      return 1;
+    }
+  }, [latency]);
+
+  return (
+    <div
+      className={`signal-strength-${signalStrength} fill-blue-400 flex flex-col items-center`}
+    >
+      <SignalSvg width={36} height={36} />
+      <span className="text-blue-400 font-bold text-xl">{latency} ms</span>
+    </div>
+  );
+}
