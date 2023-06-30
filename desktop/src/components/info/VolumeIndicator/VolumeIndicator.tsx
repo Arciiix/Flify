d7b5a2859c35bcd2ab565e928fe6e181817ca95e
Arciiix <artur.nowak100@gmail.com>
2023-06-30 14:41:47 +0200

    Create volume indicator

---
+++ desktop/src/components/info/VolumeIndicator/VolumeIndicator.tsx
@@ -0,0 +1,17 @@
+interface VolumeIndicatorProps {
+  percentage: number; // From 0 to 100
+}
+
+export default function VolumeIndicator({ percentage }: VolumeIndicatorProps) {
+  return (
+    <div className="relative flex items-center">
+      <div
+        className="absolute top-0 left-0 flex w-full h-6 rounded-2xl bg-gradient-to-r from-red-600 via-yellow-400 to-green-700 transition-all duration-75"
+        style={{
+          clipPath: `inset(0% ${100 - percentage}% 0% 0% round 10px)`,
+        }}
+      ></div>
+      <div className="flex w-full h-6 rounded-2xl bg-gradient-to-r from-red-600 via-yellow-400 to-green-700 opacity-40"></div>
+    </div>
+  );
+}
--- desktop/src/pages/App/AppPage.tsx
+++ desktop/src/pages/App/AppPage.tsx
@@ -1,7 +1,8 @@
 import HostInfo from "@/components/info/HostInfo/HostInfo";
+import VolumeIndicator from "@/components/info/VolumeIndicator/VolumeIndicator";
 import Logo from "@/components/ui/Logo/Logo";
 import { sockets } from "@/state/connection/sockets.atom";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useRecoilValue } from "recoil";
 
@@ -21,6 +22,11 @@ export default function AppPage() {
     <div className="flex flex-col items-center gap-3">
       <Logo isAnimated />
       <HostInfo />
+
+      <div className="flex flex-col w-5/12 gap-1">
+        <VolumeIndicator percentage={50} />
+        <VolumeIndicator percentage={50} />
+      </div>
       {/* TODO: Display it nicely */}
       {deviceList.map((e) => {
         return <span>{JSON.stringify(e.metadata)}</span>;
