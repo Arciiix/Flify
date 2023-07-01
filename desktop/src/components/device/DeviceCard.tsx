2d98049cfb86c7ef3b4aef9b55a9a5a84d0db3ec
Arciiix <artur.nowak100@gmail.com>
2023-07-01 19:51:57 +0200

    Add disconnect button on device card

--- desktop/src/components/device/DeviceCard.tsx
+++ desktop/src/components/device/DeviceCard.tsx
@@ -1,12 +1,18 @@
 import Battery from "../ui/Battery/Battery";
 import Signal from "../ui/Signal/Signal";
+import DisconnectButton from "./DisconnectButton";
 
 export default function DeviceCard() {
+  const handleDisconnect = () => {
+    // TODO: Handle disconnect
+    console.log("disconnect");
+  };
+
   return (
     <div className="flex flex-col bg-flify bg-opacity-20 p-4 rounded-xl w-72">
       <div className="flex justify-around">
         <Battery percentage={50} />
-
+        <DisconnectButton onDisconnect={handleDisconnect} />
         <Signal latency={50} />
       </div>
     </div>
---
+++ desktop/src/components/device/DisconnectButton.tsx
@@ -0,0 +1,18 @@
+import { MdClose } from "react-icons/md";
+
+interface DisconnetButtonProps {
+  onDisconnect: () => void;
+}
+
+export default function DisconnectButton({
+  onDisconnect,
+}: DisconnetButtonProps) {
+  return (
+    <button
+      className="bg-gray-800 border-gray-800 border-4 w-14 h-14 p-2 rounded-xl hover:bg-opacity-50 active:bg-transparent flex items-center justify-center transition-all"
+      onClick={onDisconnect}
+    >
+      <MdClose size={32} />
+    </button>
+  );
+}
