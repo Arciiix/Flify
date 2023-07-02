61cf55fd68a9cc1ad1463e7af7537a64e216dc3c
Arciiix <artur.nowak100@gmail.com>
2023-07-02 15:00:00 +0200

    Add underline animation on made with heart component

--- desktop/src/components/ui/MadeWithHeart/MadeWithHeart.tsx
+++ desktop/src/components/ui/MadeWithHeart/MadeWithHeart.tsx
@@ -1,9 +1,10 @@
 export default function MadeWithHeart() {
   // TODO: Add a cool hover effect
   return (
-    <div>
+    <div className="group">
       Made with ❤️ by{" "}
       <a
+        className="bg-left-bottom bg-gradient-to-r from-flify pb-1 to-blue-400 bg-[length:0%_2px] bg-no-repeat group-hover:bg-[length:100%_2px] transition-all duration-500 ease-out active:text-flify"
         href="https://github.com/Arciiix"
         target="_blank"
         rel="noreferrer noopener"
